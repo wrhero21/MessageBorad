b425f131aeb1fd1c88ebfaa7efb44e92e27edc90
JIE CHU <gg@JIEde-MacBook-Pro.local>
2019-06-12 14:12:17 +0800

    messageboard without localstorege

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,59 +1,115 @@
-import React from 'react';
-import './styles/App.css';
-import 'antd/dist/antd.css';
+import React from 'react'
+import 'antd/dist/antd.css'
+import { Comment, Avatar, Form, Button, List, Input } from 'antd'
+import moment from 'moment';
+import './styles/App.css'
 
 const { TextArea } = Input
 
-class Message extends React.Component{
-    render(){
-        let divStyle={marginBottom:20}
-        let messageStyle={marginLeft:20}
-        return (
-            <div style={divStyle}>
-            //把data傳傳進來後回傳
-            <div>{this.props.name}說：</div>
-            <div style={messageStyle}>{this.props.message}</div>
-            </div>
-        )
+const CommentList = ({ comments }) => (
+  <List
+    dataSource={comments}
+    header={`${comments.length} ${comments.length > 1 ? 'replies' : 'reply'}`}
+    itemLayout="horizontal"
+    renderItem={props => <Comment {...props} />}
+  />
+)
+const Editor = ({ onChange,onChangeName, onSubmit, submitting, name, value }) => (
+  <div>
+    <Form.Item>
+      <Input type="text" size="large" onChange={onChangeName} value={name} placeholder="請輸入名稱..." />       
+      <TextArea rows={4} onChange={onChange} value={value} placeholder="請輸入留言..." />
+    </Form.Item>
+    <Form.Item>
+      <Button htmlType="submit" loading={submitting} onClick={onSubmit} type="primary" >
+        留言
+      </Button>
+    </Form.Item>
+  </div>
+)
+
+/*const useLocalStorge = localStorageKey => {
+  const [value,setValue]=React.useState(
+    localStorage.getItem(localStorageKey) || ''
+  )
+  React.useEffect(()=> {
+    localStorage.setItem(localStorageKey,value)
+  },[value])
+  return [value, setValue]
+}
+*/
+
+export class App extends React.Component {
+  state = {
+    comments: [],
+    submitting: false,
+    name: '',
+    value: ''
+  };
+  
+  handleSubmit = () => {
+    if (!this.state.value) {
+      return;
     }
+
+    this.setState({
+      submitting: true,
+    });
+
+    setTimeout(() => {
+      this.setState({
+        submitting: false,
+        name: '',
+        value: '',
+        comments: [
+          {
+            author: <p>{this.state.name}</p>,
+            content: <p>{this.state.value}</p>,
+            datetime: moment().fromNow(),
+          },
+          ...this.state.comments,
+        ],
+      });
+    }, 1000);
+  };
+
+  handleChange = e => {
+    this.setState({
+      value: e.target.value,
+    });
+  };
+  handleNameChange = e => {
+    this.setState({
+      name: e.target.value
+    })
+  }
+  render() {
+    const { comments, submitting, name, value } = this.state;
+   /* const [valueLocal, setValue] = useLocalStorge(
+      this.handleNameChange
+    )
+    */
+    //console.log(valueLocal)
+
+    return (
+      <div>
+        <header>
+          <h1>Messageborad</h1>
+        </header>
+        {comments.length > 0 && <CommentList comments={comments} />}
+        <Comment
+          content={
+            <Editor
+              onChangeName={this.handleNameChange}
+              onChange={this.handleChange}
+              onSubmit={this.handleSubmit}
+              submitting={submitting}
+              name={name}
+              value={value}
+            />
+          }
+        />
+      </div>
+    );
+  }
 }
-
-class MessageBlock extends React.Component{
-    render(){
-        //用map迴圈把留言資料用props傳進Message組件，再指定給message
-        let message = this.props.messageDate.map((item) => {
-            return <Message key={item.id} name={item.name} message={item.message} />
-        })
-        return (
-            <div>
-                {/*回傳放留言資料的message變數*/}
-                {message}
-            </div>
-        )
-    }
-
-}
-
-let date = [
-  {id:'1',name:'aa',message:'嗨！大家今天過得好嗎！'},
-  {id:'2',name:'bb',message:'not bad!'},
-  {id:'3',name:'cc',message:'我過得還不錯'},
-  {id:'4',name:'aa',message:'do you finish homework ?'},
-  {id:'5',name:'bb',message:'還沒，你呢？'},]
-
-
-export const App = () => {
-  return (
-    <div>
-      /*透過this.props將data傳入MessageBlock處理 */
-      <MessageBlock messageDate={this.props.messageDate}/>
-    </div>
-  );
-}
-
-
-ReactDOM.render(<App messageDate = {date} />
-  ,document.getElementById('root'))
-
-
-
